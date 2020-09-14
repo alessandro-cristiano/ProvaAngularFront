a32f8266b77d752bf57bdd27cfb00bfffac93a3a
alessandro-cristiano <alexcris22@hotmail.it>
2020-09-14 16:51:37 +0200

    lista 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent}  from './home/home.component';
+import {UserComponent}  from './user/user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: '', pathMatch: 'full', redirectTo: 'home' },
+                        {path:"home",component:HomeComponent},
+                        {path:"lista", component:UserComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
